938686b4f6a1ce60372af5772a505bd315fc255d
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-02-06 01:54:31 +0000

    Added shell script `pbsCall_NonReferenceL1HS_bix2.sh` to call script `Call_NonReferenceL1HS_bix2.R'

---
+++ Scripts/pbsCall_NonReferenceL1HS_bix2.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+#$ -N Call_NonReferenceL1HS_bix2
+#
+#$ -cwd
+#$ -l h_rt=300:00:00
+#
+#$ -j y
+#
+#$ -S /bin/bash
+#
+/share/diskarray1/Updated_RNA_software/R/R-3.2.1-download/bin/Rscript Call_NonReferenceL1HS_bix2.R 
