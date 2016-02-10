6ebd4f7b38d50d6ecea57917e7ea7720a4498367
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-02-10 20:24:06 +0000

    Added scripts `Read_dbRIP.R` that reads data from dbRIP and `ProcessHotL1Table.R` that processes a table about hot L1 from Brouha eta al. (2003). I also reduced the cutoff for a suspected non-reference peak in `Call_NonReferenceL1HS_bix2.R`.

--- Scripts/Create_L1HS_table.R
+++ Scripts/Create_L1HS_table.R
@@ -38,13 +38,14 @@ SeqOutfileName_L1HS <- "D:/L1polymORF/Data/L1Sequences_reptab.fas"
 TabOutfileName_L1   <- "D:/L1polymORF/Data/L1_repeat_table.csv"
 SeqOutfileName_L1   <- "D:/L1polymORF/Data/L1AllSequences_reptab.fas"
 
-
 #######################################
 #                                     #
 #     Read bed file and               #
 #   save fasta file with sequences    #
 #                                     #
 #######################################
+
+cat("Read and process repeatMasker table \n")
 
 # Read repeat table
 RepeatTable <- read.delim("D:/L1polymORF/Data/repeatsHg38")
@@ -56,9 +57,7 @@ RepeatTable <- RepeatTable[nchar(as.character(RepeatTable$genoName)) <= 5, ]
 
 # Subset to get only L1HS rows and save
 L1HSTable <- RepeatTable[RepeatTable$repName == "L1HS",]
-
-# Subset to retain only strandard genome names
-write.csv(L1HSTable, TabOutfileName_L1HS)
+idxFullLenghtL1HS <- which(abs(L1HSTable$genoEnd - L1HSTable$genoStart) >= 6000)
 
 # Make some corrections to create a proper GRanges object with L1 Seqences
 L1HSIRanges <- IRanges(start = L1HSTable$genoStart,
@@ -68,6 +67,7 @@ L1HSGRanges <- GRanges(seqnames = L1HSTable$genoName, ranges = L1HSIRanges,
 L1HSGRanges <- L1HSGRanges[width(L1HSGRanges) >= 6000]
 
 # Get all L1 sequences  
+cat("Get sequences for L1 element \n")
 L1HSSeq <- getSeq(BSgenome.Hsapiens.UCSC.hg38, L1HSGRanges, as.character = T)
 L1HSSeqNames <- paste(as.vector(seqnames(L1HSGRanges)), start(L1HSGRanges), end(L1HSGRanges),
       strand(L1HSGRanges), sep = "_")
@@ -82,9 +82,8 @@ write.fasta(L1HSList, L1HSSeqNames, SeqOutfileName_L1HS)
 
 # Subset to get only L1 rows and save
 L1Table <- RepeatTable[RepeatTable$repFamily == "L1",]
-
-# Subset to retain only strandard genome names
-write.csv(L1Table, TabOutfileName_L1)
+idxL1HS_inFullTable <- which(abs(L1Table$genoEnd - L1Table$genoStart) >= 6000 &
+                                L1Table$repName == "L1HS")
 
 # Make some corrections to create a proper GRanges object with L1 Seqences
 L1IRanges <- IRanges(start = L1Table$genoStart,
@@ -102,3 +101,56 @@ L1SeqNames <- paste(as.vector(seqnames(L1GRanges)), start(L1GRanges), end(L1GRan
 L1List <- lapply(L1Seq, s2c)
 write.fasta(L1List, L1SeqNames, SeqOutfileName_L1)
 
+#######################################
+#                                     #
+#     Process hot L1 table            #
+#                                     #
+#######################################
+
+# Read in table of hot L1 (obtained from Brouha et al.2003 PNAS)
+cat("Read table with hot L1s \n")
+L1Hot_raw <- read.delim('D:/L1polymORF/Data/L1HotTable_raw.txt', sep = " ", as.is = T)
+
+# Find duplicated column names 
+CNames        <- colnames(L1Hot_raw)
+ColNameSuffix <- substr(CNames, nchar(CNames) - 1, nchar(CNames))
+blnDuplCols   <- ColNameSuffix == ".1"
+
+# Separate table according to duplicated column names and append duplucated part
+L1HotDupl <- L1Hot_raw[,blnDuplCols]
+colnames(L1HotDupl) <- CNames[!blnDuplCols]
+L1HotTable    <- rbind(L1Hot_raw[,!blnDuplCols],L1HotDupl)
+
+# Download sequences 
+cat("Get sequences for hot L1 loci \n")
+choosebank("genbank")
+L1SeqHot <- sapply(L1HotTable$Accession_no., function(AccNr){
+  x <- query(listname = "L1", paste("AC=", AccNr, sep = ""))
+  Seq <- getSequence(x$req)
+  paste(Seq[[1]], collapse = "")
+})
+closebank()
+
+# Match each L1HS to a locus 
+cat("Match hot L1 loci to L1s from repeatMasker\n")
+L1SeqHot <- toupper(L1SeqHot)
+GrepMatch <- sapply(L1HSSeq, function(x) {
+  idxMatch <- grep(substr(x, 1, 500), L1SeqHot, ignore.case = T)
+  if (length(idxMatch) == 0) NA else idxMatch
+})
+
+# Match info from L1SeqHot table to L1HSTable
+L1HSTable[,c("Group", "Allele_frequency.", "Act_L1rp")] <- NA
+L1HSTable[idxFullLenghtL1HS, c("Group", "Allele_frequency.", "Act_L1rp")] <- 
+  L1HotTable[GrepMatch,c("Group", "Allele_frequency.", "Act_L1rp")]
+
+# Match info from L1SeqHot table to L1HSTable
+L1Table[,c("Group", "Allele_frequency.", "Act_L1rp")] <- NA
+L1Table[idxL1HS_inFullTable, c("Group", "Allele_frequency.", "Act_L1rp")] <- 
+  L1HotTable[GrepMatch,c("Group", "Allele_frequency.", "Act_L1rp")]
+
+# Write out L1HSTable
+write.csv(L1HSTable, TabOutfileName_L1HS)
+
+# Write out L1Table
+write.csv(L1Table, TabOutfileName_L1)
---
+++ Scripts/Read_dbRIP.R
@@ -0,0 +1,2 @@
+# The following script reads a table from the dbRIP database
+dbRIP <- read.delim("D:/L1polymORF/Data/L1_hg19_v2h.txt", header = F)
