392be0f40075f396a4b28c54a16e09f40122ff4f
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-02-17 04:17:52 +0000

    Added script to get non-reference L1 from pacbio data.

--- Scripts/readVCF_example.R
+++ Scripts/readVCF_example.R
@@ -1,6 +1,9 @@
 library("VariantAnnotation")
+library("VariantTools")
 library("seqinr")
 
+source("https://bioconductor.org/biocLite.R")
+biocLite("VariantTools")
 
 GenomeList <- read.fasta("D:/L1polymORF/Data/Homo_sapiens_L1_consensus.fa")
 Genome <- paste(GenomeList[[1]], collapse = "")
@@ -8,5 +11,18 @@ names(Genome) <- names(GenomeList)
 SampleVCF <- readVcf(file = "D:/L1polymORF/Data/chr3_1161_aln.vcf", 
                      genome = Genome)
 VCFDF <- as.data.frame(SampleVCF@fixed)
-geno(SampleVCF)
+
+gVCF <- geno(SampleVCF)
+
+PLDF <- as.data.frame(gVCF$PL)
+
+sapply(geno(SampleVCF), class)
 snpSummary(SampleVCF)
+
+as.data.frame(info(header(SampleVCF)))
+info(SampleVCF)
+head(rowRanges(SampleVCF))
+
+pass(SampleVCF)
+any(qual(SampleVCF) != 0)
+qual
