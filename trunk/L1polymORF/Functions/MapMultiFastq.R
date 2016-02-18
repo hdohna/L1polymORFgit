d8492e24c257ecd036a7f8dab1743cc25964b66a
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-02-18 17:35:04 +0000

    Added script `MapMultiFastq_PacbioNonRefL1.R` that maps multiple fastq files to reference L1. Added function `MapMultiFastq.R` that maps all fastq files in a folder to a common reference.

---
+++ trunk/L1polymORF/Functions/MapMultiFastq.R
@@ -0,0 +1,51 @@
+##############################################
+#
+# General description:
+#
+#   The following function aligns all fastq files in a folder
+#   to the same reference using bwa
+
+# Input:
+#
+#     FastQFolder: character string providing path to folder that contains 
+#          the fastq files to be mapped
+#     Reference: character string providing path to file containing the 
+#          common reference
+#     AlignCommand: text string providing the alignment command (options can be
+#          added here)
+#     SamSuffix: suffix for sam files created by alignment
+
+
+# Output:
+#   
+#     ...
+
+##############################################
+
+MapMultiFastq <- function(FastQFolder, Reference, 
+                              AlignCommand = '/home/txw/bwa/bwa-0.7.12/bwa mem',
+                              SamSuffix = "_aln.sam") {
+  
+  # Bam suffices for alignment files created 
+  BamSuffix <- paste(substr(SamSuffix, 1, nchar(SamSuffix) - 4), ".bam", sep = "")
+  
+  cat("*******   Mapping little fastq files in", FastQFolder, "...   *******\n")
+  
+  # Get all paths to fastq files in the folder
+  FastQPaths <- list.files(FastQFolder, pattern = ".fastq", full.names = T)
+  
+  # Create index file
+  CmdIndex <- paste('bwa index', Reference)
+  system(CmdIndex)
+  
+  # Run BWA for each little fastq file  
+  OutFiles <- paste(substr(FastQPaths, 1, nchar(FastQPaths) - 6), SamSuffix, sep = "")
+  CmdLines <- paste(AlignCommand,  Reference, FastQPaths)
+  CmdLines <- paste(CmdLines, OutFiles, sep = " > ")
+  for (CmdL in CmdLines) system(CmdL)
+  
+  # Return paths to fastq files and sam files
+  cbind.data.frame(FastQPath = FastQPaths, SamPath = OutFiles)
+}
+
+
