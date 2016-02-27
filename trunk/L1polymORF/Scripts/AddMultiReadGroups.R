f520bcde62a8e6b1dbd4350a17638ad4bbf3b0cd
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-02-27 00:42:40 +0000

    Minor bugfix in script `AddMultiReadGroups.R` that adds read groups to each bam file in a folder.

--- trunk/L1polymORF/Scripts/AddMultiReadGroups.R
+++ trunk/L1polymORF/Scripts/AddMultiReadGroups.R
@@ -4,7 +4,7 @@
 BamFolder <- "/home/hzudohna/L1polymORF/Data/PacbioFastqPerSuspectPeak/"
 
 # Picard command
-PicardCmd   <- "java -jar /home/txw/picard/picard-tools-1.52/picard.jar AddOrReplaceReadGroups"
+PicardCmd   <- "java -jar /home/txw/picard/picard-tools-1.52/picard-1.52.jar AddOrReplaceReadGroups"
 OptionLines <- c("RGLB=lib1", "RGPL=illumina", "RGPU=unit1", "RGSM=20")
 
 # Get all paths to fastq files in the folder
