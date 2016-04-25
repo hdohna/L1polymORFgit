0ea19a1c40660948f2f3d8396203827a2dc95e54
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-04-25 23:54:45 +0000

    Added the script `run_AlignmentWorkFlow_PacBio` that runs the function `AlignmentWorkFlow.R` for the PacBio dataset alinged to catalogue.

---
+++ Scripts/run_AlignmentWorkFlow_PacBio.R
@@ -0,0 +1,13 @@
+# The script below runs the function 'AlignmentWorkflow'
+
+# Source start script
+source('/home/hzudohna/L1polymORF/Scripts/_Start_L1polymORF_bix2.R')
+
+# Run function
+AlignmentWorkflow(FastqFile = "", 
+                  ReferenceFile = "", 
+                  SamFile = '/share/diskarray3/hzudohna/ NA12878PacBio_aln2Catalogue.sam',
+                  blnAlign = F,
+                  blnSam2SortedBam = T,
+                  blnDedup = T,
+                  blnSort = T)
