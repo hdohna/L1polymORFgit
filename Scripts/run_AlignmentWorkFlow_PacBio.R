287cb580644424b85db9463d5a1fd618deac7945
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-04-26 22:46:01 +0000

    Corrected file path in`run_AlignmentWorkFlow_PacBio.R`.

--- Scripts/run_AlignmentWorkFlow_PacBio.R
+++ Scripts/run_AlignmentWorkFlow_PacBio.R
@@ -6,7 +6,7 @@ source('/home/hzudohna/L1polymORF/Scripts/_Start_L1polymORF_bix2.R')
 # Run function
 AlignmentWorkflow(FastqFile = "", 
                   ReferenceFile = "", 
-                  SamFile = '/share/diskarray3/hzudohna/ NA12878PacBio_aln2Catalogue.sam',
+                  SamFile = '/share/diskarray3/hzudohna/NA12878PacBio_aln2Catalogue.sam',
                   blnAlign = F,
                   blnSam2SortedBam = T,
                   blnDedup = T,
