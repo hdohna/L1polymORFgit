81626da9d7ef42550d2d397826e74a0599e5c802
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-05-11 21:00:37 +0000

    Updated new file names in scripts `run_AlignmentWorkFlow_Capture.R` and `run_AlignmentWorkFlow_PacBio.R`.

--- trunk/L1polymORF/Scripts/run_AlignmentWorkFlow_Capture.R
+++ trunk/L1polymORF/Scripts/run_AlignmentWorkFlow_Capture.R
@@ -6,7 +6,7 @@ source('/home/hzudohna/L1polymORF/Scripts/_Start_L1polymORF_bix2.R')
 # Run function
 AlignmentWorkflow(FastqFile = "", 
                   ReferenceFile = "", 
-                  SamFile = '/share/diskarray3/hzudohna/NA12878-L15P_S1_L001_001_Catalogue.sam',
+                  SamFile = '/share/diskarray3/hzudohna/NA12878-L15P_S1_aln2Catalogue2016-05-07.sam',
                   blnAlign = F,
                   blnSam2SortedBam = T,
                   blnDedup = T,
