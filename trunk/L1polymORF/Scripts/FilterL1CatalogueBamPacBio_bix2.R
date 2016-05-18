403a8faf5701d7eb60d4639e107edabf60cca571
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-05-18 00:15:17 +0000

    Added script `FilterL1CatalogueBamPacBio_bix2.R` that filters PacBio reads aligned to the L1 catalog.

--- trunk/L1polymORF/Scripts/FilterL1CatalogueBamPacBio_bix2.R
+++ trunk/L1polymORF/Scripts/FilterL1CatalogueBamPacBio_bix2.R
@@ -7,23 +7,15 @@
 #                                           #
 #############################################
 
-# Source start script
-#source('/home/hzudohna/L1polymORF/Scripts/_Start_L1polymORF_bix2.r')
-
 # Load packages
 library(BSgenome.Hsapiens.UCSC.hg38)
 library(csaw)
 library(ShortRead)
 library(Rsamtools)
 
-# Specify run parameters
-blnCreateAlignList <- F
-
 # Specify file paths
-BamFile <- '/share/diskarray3/hzudohna/NA12878-L15P_S1_aln2Catalogue2016-05-07.dedup.unique.sorted.bam'
-FilteredBamFile <- '/share/diskarray3/hzudohna/NA12878-L15P_aln2Catalogue2016-05-07_filtered.bam'
-CatalogueFile <- "/home/hzudohna/L1polymORF/Data/L1Catalogue_Updated_Sat_May_07_15-15-31_2016.csv"
-AlignListFile <- "/home/hzudohna/L1polymORF/Data/L1CatalogueWithFlank_Sat_May_07_15-15-31_2016_L1Locations.RData"
+BamFile <- '/share/diskarray3/hzudohna/NA12878PacBio_aln2Catalogue2016-05-07sortedunique.sorted.bam'
+FilteredBamFile <- '/share/diskarray3/hzudohna/NA12878PacBio_aln2Catalogue2016-05-07filtered.bam'
 
 ############################
 #                          #
@@ -31,12 +23,8 @@ AlignListFile <- "/home/hzudohna/L1polymORF/Data/L1CatalogueWithFlank_Sat_May_07
 #                          #
 ############################
 
-# Load L1 catalogue and files that indicate where in the alignment the L1 
-# insertion is located
-#load(file = AlignListFile)
-
 cat("Filtering bam file ...\n")
-paramFilter  <- ScanBamParam(tagFilter = list(NM = 0:4),
+paramFilter  <- ScanBamParam(tagFilter = list(AS = 5000:1000),
                              mapqFilter = 1,
                              scanBamFlag(isUnmappedQuery = F))
 filterBam(BamFile, FilteredBamFile, param = paramFilter)
