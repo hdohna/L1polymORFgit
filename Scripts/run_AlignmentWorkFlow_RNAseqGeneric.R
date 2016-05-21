63b94462b497e65f971395f8330c4f880b4e39ad
hdohna <hdohna@afbc809c-dade-485d-ac0a-f532d28bc5c4>
2016-05-21 00:35:55 +0000

    Added new script `run_AlignmentWorkFlow_RNAseqGeneric.R` that allows specifying the input file as a command line argument.

---
+++ Scripts/run_AlignmentWorkFlow_RNAseqGeneric.R
@@ -0,0 +1,17 @@
+# The script below runs the function 'AlignmentWorkflow'
+
+# Collect command arguments (it is assumed to contain only the sam file name)
+args <- commandArgs(trailingOnly = TRUE)
+
+# Source start script
+source('/home/hzudohna/L1polymORF/Scripts/_Start_L1polymORF_bix2.R')
+
+# Run function
+AlignmentWorkflow(FastqFile = "", 
+                  ReferenceFile = "", 
+                  SamFile = args[1],
+                  blnAlign = F,
+                  blnSam2SortedBam = T,
+                  blnDedup = F,
+                  blnRemMapQ0 = T,
+                  blnSort = T)
