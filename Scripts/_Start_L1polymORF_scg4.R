3426c1dde1a0b1acf3169696b9fba328cd449d0f
hdohna <heinrich.dohna@gmail.com>
2016-07-06 18:54:38 -0700

    Added start script for SCG4
    Added start script for SCG4

---
+++ Scripts/_Start_L1polymORF_scg4.R
@@ -0,0 +1,33 @@
+##############################################
+#
+# General description:
+#
+#   The following script loads standard packages and functions to be used in
+#   the L1polymORF project
+
+# Input:
+#
+#   
+
+# Output:
+#   
+#    : 
+
+##############################################
+
+########################################
+#                                      #
+#  Source packages and set parameters  #
+#                                      #
+########################################
+
+# Source all functions from GeneralRFunctions folder
+# AllFunctions <- list.files(path = "/home/hzudohna/GeneralRFunctions/", 
+#                            pattern = ".[rR]", full.names = T)
+# sapply(AllFunctions, source)
+
+# Source all functions from Functions folder
+AllFunctions <- list.files(path = "/home/hzudohna/L1polymORFgit/Functions/", 
+                           pattern = ".[rR]", full.names = T)
+sapply(AllFunctions, source)
+
