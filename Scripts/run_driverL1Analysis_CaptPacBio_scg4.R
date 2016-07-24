7993512748089c3359dffceb7a8240e927df5b7c
hdohna <heinrich.dohna@gmail.com>
2016-07-24 14:52:38 -0700

    Added script `run_driverL1Analysis_CaptPacBio_scg4.R`
    Added script `run_driverL1Analysis_CaptPacBio_scg4.R` that runs the
    function `driverL1Analysis` on SCG4.

---
+++ Scripts/run_driverL1Analysis_CaptPacBio_scg4.R
@@ -0,0 +1,34 @@
+# The script below runs the function 'driverL1Analysis'
+
+# Source start script
+source('/home/hzudohna/L1polymORFgit/Scripts/_Start_L1polymORF_scg4.R')
+
+# Load packages
+library(ShortRead)
+library(csaw)
+
+# Run function
+driverL1Analysis(
+  PeakBam = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/BZ_NA12878L1capt5-9kb_readsofinsert_hg19.bam", 
+  L1HSBamFile = NULL, 
+  FastQFolder = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/", 
+  L1HSConsensus = "/srv/gsfs0/projects/levinson/hzudohna/RefSeqData/Homo_sapiens_L1_consensus.fa",
+  L1RefRanges    = '/srv/gsfs0/projects/levinson/hzudohna/RefSeqData/L1RefRanges_hg19.Rdata',
+  OutputFolder = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/", 
+  ResultFileName = "BZ_NA12878L1capt5-9kb_Results.Rdata",
+  MinMaxCover = 5, 
+  MinGap = 6000, 
+  MinDist2L1 = 20000, 
+  blnComparePeaksWithRefL1 = T,
+  blnWriteFastq     = T,
+  blnMap2L1         = T, 
+  blnAddReadGroups  = T, 
+  blnCreateBamIndices = F,
+  blnCallHaplotypes = F, 
+  blnAnalyze        =   T,
+  AlignCommand = c('module load bwa', 'bwa mem -k17 -W40 -r10 -A2 -B5 -O2 -E1 -L0'),
+  ReadGroupSuffix = "withRG.bam",
+  BamSuffix = "withRG.bam",
+  HapTypeCallOptions = "",
+  BamSuffixHapTypeCall = "withRG.bam"
+)
