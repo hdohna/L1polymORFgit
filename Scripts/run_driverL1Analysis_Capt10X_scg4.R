c4bf5efa2360514d5394bd43f03496dcaa1b3681
hdohna <heinrich.dohna@gmail.com>
2016-07-26 14:50:00 -0700

    Added script `run_driverL1Analysis_Capt1-X_scg4.R`
    script runs analysis workflow for L1 insertion detection in 10X data.

---
+++ Scripts/run_driverL1Analysis_Capt10X_scg4.R
@@ -0,0 +1,37 @@
+# The script below runs the function 'driverL1Analysis'
+
+# Source start script
+source('/home/hzudohna/L1polymORFgit/Scripts/_Start_L1polymORF_scg4.R')
+
+# Load packages
+library(ShortRead)
+library(csaw)
+
+# Run function
+driverL1Analysis(
+  PeakBam = "/srv/gsfs0/projects/levinson/hzudohna/10Xcapture/NA12878_capt10X_aln2hg19.bam", 
+  L1HSBamFile = NULL, 
+  FastQFolder = "/srv/gsfs0/projects/levinson/hzudohna/10Xcapture/FastqFiles/", 
+  L1HSConsensus = "/srv/gsfs0/projects/levinson/hzudohna/RefSeqData/Homo_sapiens_L1_consensus.fas",
+  L1RefRanges    = '/srv/gsfs0/projects/levinson/hzudohna/RefSeqData/L1RefRanges_hg19.Rdata',
+  OutputFolder = "/srv/gsfs0/projects/levinson/hzudohna/10Xcapture/", 
+  ResultFileName = "NA12878_capt10X_Results.Rdata",
+  PlotFolder = "/srv/gsfs0/projects/levinson/hzudohna/10Xcapture/Figures/", 
+  MinMaxCover = 1, 
+  MinGap = 1000, 
+  MinDist2L1 = 20000, 
+  blnComparePeaksWithRefL1 = F,
+  blnWriteFastq     = F,
+  blnMap2L1         = F, 
+  blnAddReadGroups  = F, 
+  blnCreateBamIndices = T,
+  blnCallHaplotypes = F, 
+  blnAnalyze        =   T,
+  IdChar2Remove = 4,
+  AlignCommand = c('module load bwa', 'bwa mem'),
+  IndexCommand = c('module load bwa', 'bwa index'),
+  ReadGroupSuffix = "withRG.bam",
+  BamSuffix = "withRG.bam",
+  HapTypeCallOptions = "",
+  BamSuffixHapTypeCall = "withRG.bam"
+)
