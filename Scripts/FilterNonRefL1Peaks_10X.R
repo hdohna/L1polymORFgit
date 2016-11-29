ed72129ab31ac156481f2ea135f4d30b7fb3adae
hdohna <heinrich.dohna@gmail.com>
2016-11-29 18:25:13 +0200

    Added script `FilterNobRefL1Peaks_10X.R`
    Script filters bam file to retain reads mapping far away from reference
    L1.

---
+++ Scripts/FilterNonRefL1Peaks_10X.R
@@ -0,0 +1,12 @@
+# The following script filters a 10X bam file to keep all reads that intersect
+# with peaks away from reference L1
+load("/srv/gsfs0/projects/levinson/hzudohna/10Xcapture/NA12878_L1Ranges10X.RData")
+
+cat("Filtering NA12878_capt10X_aln2hg19.bam\n")
+cat("Saving filtered file as NA12878_capt10X_aln2hg19_NonRefL1filtered.bam\n")
+cat("in folder /srv/gsfs0/projects/levinson/hzudohna/10Xcapture/ \n")
+
+param <- ScanBamParam(which = SuspectL1Ranges, what = scanBamWhat())
+filterBam(file = "/srv/gsfs0/projects/levinson/hzudohna/10Xcapture/NA12878_capt10X_aln2hg19.bam", 
+          destination = "/srv/gsfs0/projects/levinson/hzudohna/10Xcapture/NA12878_capt10X_aln2hg19_NonRefL1filtered.bam", 
+          param = param)
