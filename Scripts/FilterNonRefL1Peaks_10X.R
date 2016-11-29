f6525306e19333ea31a46ffdb79d555bf00e3824
hdohna <heinrich.dohna@gmail.com>
2016-11-29 18:28:52 +0200

    Added loading of library to `FilterNonRefL1Peaks_10X.R`

--- Scripts/FilterNonRefL1Peaks_10X.R
+++ Scripts/FilterNonRefL1Peaks_10X.R
@@ -1,5 +1,7 @@
 # The following script filters a 10X bam file to keep all reads that intersect
 # with peaks away from reference L1
+library(Rsamtools)
+
 load("/srv/gsfs0/projects/levinson/hzudohna/10Xcapture/NA12878_L1Ranges10X.RData")
 
 cat("Filtering NA12878_capt10X_aln2hg19.bam\n")
