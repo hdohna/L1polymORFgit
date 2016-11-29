dfdb880b1eed0f3465c43b32d7792be1c9e5dd44
hdohna <heinrich.dohna@gmail.com>
2016-11-29 18:33:13 +0200

    Added script `CheckPeakConsistency`
    Script checks peaks that are shared by 10X and PacBio experiment.

---
+++ Scripts/CheckPeakConsistency.R
@@ -0,0 +1,22 @@
+load("D:/L1polymORF/Data/AnalyzedPacBioL1Ranges.RData")
+ObjectList <- ls()
+for (ObjectName in ObjectList){
+  NewName <- paste(ObjectName, "PacBio", sep = "_")
+  assign(NewName, eval(parse(text = ObjectName)),
+         envir = .GlobalEnv)
+}
+
+load("D:/L1polymORF/Data/NA12878_L1Ranges10X.RData")
+for (ObjectName in ObjectList){
+  NewName <- paste(ObjectName, "10X", sep = "_")
+  assign(NewName, eval(parse(text = ObjectName)),
+         envir = .GlobalEnv)
+}
+IslGRanges_reduced_10X
+IslGRanges_reduced_PacBio
+
+OverlapRanges <- subsetByOverlaps(IslGRanges_reduced_PacBio[idxSuspectL1Ranges_PacBio], 
+                 IslGRanges_reduced_10X[idxSuspectL1Ranges_10X])
+ 
+export.bed(OverlapRanges, "D:/L1polymORF/Data/OverlapPacBio10X_hg19")
+
