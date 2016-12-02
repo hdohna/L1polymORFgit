0d054d9915ee256bf6409647a4ebd6a3da2b1489
hdohna <hb54@aub.edu.lb>
2016-12-02 18:18:13 +0200

    Added function `ReadLengthFromCigar`
    Function calculates length of a mapped read based on cigar string

---
+++ Functions/ReadLengthFromCigar.R
@@ -0,0 +1,24 @@
+# General description:
+#
+#    This function calculates the length of a mapped read from the cigar string
+
+# Arguments:
+#   
+#    CigarString: sam file cigar string
+
+# Output:
+#   
+#    number of bp clipped (from both sides)
+
+# Comment:
+
+ReadLengthFromCigar <- function(CigarString){
+  Nrs <- as.numeric(strsplit(CigarString, "[A-Z]")[[1]])
+  Ltrs <- strsplit(CigarString, "[0-9]")[[1]]
+  Ltrs <- Ltrs[Ltrs != ""]
+  blnPlus  <- Ltrs %in% c("M", "D")
+  sum(Nrs[blnPlus])
+}
+
+
+
