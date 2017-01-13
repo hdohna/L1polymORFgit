ae1c3c5e78d09bf84e644b0af3861db57f54397f
hdohna <hb54@aub.edu.lb>
2017-01-13 18:50:19 +0200

    Added function `NrClippedFromCigar.R`
    Function determines number of left and right clipped bp from cigar
    string.

--- Functions/NrClippedFromCigar.R
+++ Functions/NrClippedFromCigar.R
@@ -8,17 +8,17 @@
 
 # Output:
 #   
-#    number of bp clipped (from both sides)
+#    vector (length 2) of number of left and right bp clipped 
 
 # Comment:
 
 NrClippedFromCigar <- function(CigarString){
   Nrs <- as.numeric(strsplit(CigarString, "[A-Z]")[[1]])
   Nrs <- Nrs[c(1, length(Nrs))]
-  Ltrs <- strsplit(CigarString, "[1-9]")[[1]]
+  Ltrs <- strsplit(CigarString, "[0-9]")[[1]]
   Ltrs <- Ltrs[Ltrs != ""]
   Ltrs <- Ltrs[c(1, length(Ltrs))]
-  (Ltrs == "S") %*% Nrs
+  (Ltrs == "S") * Nrs
 }
 
 
