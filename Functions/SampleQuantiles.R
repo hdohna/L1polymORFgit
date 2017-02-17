29f7b6bf23a7e1a1c41a751db7389069685640c2
hdohna <heinrich.dohna@gmail.com>
2017-02-17 11:16:45 +0200

    Added sampled means to function `SampleQuantiles.R`

--- Functions/SampleQuantiles.R
+++ Functions/SampleQuantiles.R
@@ -19,6 +19,7 @@
 #   
 #    QMat: matrix of observed qunatiles (1st row), lower qunatile (2nd row) and
 #          upper quantile (3rd row) for each sampled quantile
+#    SampleMeans: vector of sampled means
 
 ##############################################
 
@@ -27,15 +28,17 @@
 ######
 SampleQuantiles <- function(Vals, SubsetSize, NrSamples = 1000, 
                             QuantV = seq(0, 1, 0.1), LowerQ = 0.025, UpperQ = 0.975){
+  SampleMeans <- rep(NA, NrSamples)
   SampledQMat <- matrix(nrow = NrSamples,ncol = length(QuantV))
   for (i in 1:NrSamples){
     SampleVals <- sample(Vals, SubsetSize)
+    SampleMeans[i]  <- mean(SampleVals)
     SampledQMat[i,] <- quantile(SampleVals, QuantV)
   }
   QMat <- matrix(nrow = 3, ncol = length(QuantV))
   QMat[1,] <- quantile(Vals, QuantV)
   QMat[2:3,] <- apply(SampledQMat, 2, 
                       FUN = function(x) quantile(x, c(LowerQ, UpperQ)))
-  QMat
+  list(QMat = QMat, SampleMeans = SampleMeans)
 }
   
