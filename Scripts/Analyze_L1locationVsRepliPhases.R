dd2ed95bc0e85aee3630fb5793b2a94de88d36b4
hdohna <hb54@aub.edu.lb>
2017-02-21 15:14:45 +0200

    Changes in `Analyze_L1locationVsRepliPhase.R`

--- Scripts/Analyze_L1locationVsRepliPhases.R
+++ Scripts/Analyze_L1locationVsRepliPhases.R
@@ -6,6 +6,9 @@ library(Rsamtools)
 library(rtracklayer)
 library(Hotelling)
 
+# Source start script
+source('D:/L1polymORFgit/Scripts/_Start_L1polymORF.r')
+
 ############
 #  Process L1 data
 ############
@@ -47,12 +50,16 @@ L1CatGR_hg19 <- unlist(L1CatGR_hg19[idxUniqueMapped])
 L1CatGR_hg19 <- L1CatGR_hg19[width(L1CatGR_hg19) > 6000]
 
 # Find overlap with fragments and catalog elements
-L1RefGR_fragm    <- L1RefGR[width(L1RefGR) < 5000]
+L1RefGR_fragm    <- L1RefGR[width(L1RefGR) < 6000]
 
 # Get combined genomic ranges and create a bed file
 L1GR_combined <- c(L1CatGR_hg19, L1RefGR_fragm)
 L1GR_combinedDF <- as.data.frame(L1GR_combined)
-write.table(L1GR_combinedDF[1:1000,1:3], "D:/L1polymORF/Data/L1GRtable",
+write.table(L1GR_combinedDF[1:1000,1:3], "D:/L1polymORF/Data/L1GRtable1",
+            quote = F, row.names = F, col.names = F)
+write.table(L1GR_combinedDF[1001:nrow(L1GR_combinedDF),1:3], "D:/L1polymORF/Data/L1GRtable2",
+            quote = F, row.names = F, col.names = F)
+write.table(L1GR_combinedDF[,1:3], "D:/L1polymORF/Data/L1GRtableFull",
             quote = F, row.names = F, col.names = F)
 
 ############
@@ -60,11 +67,13 @@ write.table(L1GR_combinedDF[1:1000,1:3], "D:/L1polymORF/Data/L1GRtable",
 ############
 
 # Read table with open compartment data
-RepliTableFiles <- list.files("D:/L1polymORF/Data/", 
-   pattern = "UWrepliSeqGM12878_L1GR", full.names = T)
-RepliTable <- read.table(RepliTableFiles[1], skip = 1)
+RepliTableFiles <- list.files("D:/L1polymORF/Data/UWrepliSeqData", 
+   pattern = "UWrepliSeqGM12878_L1GRall", full.names = T)
+# RepliTableFiles <- list.files("D:/L1polymORF/Data/", 
+#                               pattern = "UWrepliSeqGM12878_L1GR", full.names = T)
+# RepliTableFiles <- RepliTableFiles[-1]
 
-FileName   <- RepliTableFiles[2]
+# Function to make a replication phase table
 makeRepliTable <- function(FileName) {
   NameSplt   <- strsplit(FileName, "_")[[1]]
   RepliPhase <- NameSplt[length(NameSplt)]
@@ -96,8 +105,10 @@ makeRepliTable <- function(FileName) {
   colnames(RepliAgg)[colnames(RepliAgg) == "PhasePercent"] <- RepliPhase
   return(RepliAgg[ ,colnames(RepliAgg) != "ID"])
 }
-RepliAgg <- makeRepliTable(RepliTableFiles[2])
-for (FileName in RepliTableFiles[-c(1:2)]){
+
+# Create an aggregated replication table
+RepliAgg <- makeRepliTable(RepliTableFiles[1])
+for (FileName in RepliTableFiles[-1]){
   NewTable <- makeRepliTable(FileName)
   RepliAgg <- merge(RepliAgg, NewTable)
 }
@@ -106,27 +117,60 @@ for (FileName in RepliTableFiles[-c(1:2)]){
 RepliGR          <- makeGRangesFromDataFrame(RepliAgg)
 idxOverlap_fragm <- findOverlaps(RepliGR, L1RefGR_fragm)
 idxOverlap_Cat   <- findOverlaps(RepliGR, L1CatGR_hg19)
+idxOverlap_fragm@from
+max(idxOverlap_Cat@to)
+
+# Get 2 replication matrices, 1 for fragments and one for full L1
+OrderedPhases <- c("G1b", "S1", "S2", "S3", "S4", "G2")
+#OrderedPhases <- c("S1", "S2", "S3", "S4", "G2")
+RepliFragm <- RepliAgg[idxOverlap_fragm@from, OrderedPhases]
+RepliFull  <- RepliAgg[idxOverlap_Cat@from, OrderedPhases]
+StdErr_Fragm <- apply(RepliFragm, 2, FUN = function(x) sqrt(var(x) / length(x)))
+StdErr_Full  <- apply(RepliFull, 2, FUN = function(x) sqrt(var(x) / length(x)))
+plot(colMeans(RepliFragm), type = "l", col = "red", xaxt = "n", 
+     ylim = c(5, 27), xlab = "Replication phase", ylab = "Percentage")
+axis(1, at = seq_along(OrderedPhases), labels = OrderedPhases)
+lines(colMeans(RepliFull), col = "blue")
+AddErrorBars(MidX = seq_along(OrderedPhases), MidY = colMeans(RepliFragm),
+             ErrorRange = StdErr_Fragm, TipWidth = 0.02, Col = "red")
+AddErrorBars(MidX = seq_along(OrderedPhases), MidY = colMeans(RepliFull),
+             ErrorRange = StdErr_Full, TipWidth = 0.02, Col = "blue")
+FitDataRepliFragm <- matrix(nrow = nrow(RepliFragm) * length(OrderedPhases), ncol = 2)
+FitDataRepliFull  <- matrix(nrow = nrow(RepliFull) * length(OrderedPhases), ncol = 2)
+for (i in 1:length(OrderedPhases)){
+  idxFragm <- ((i - 1) * nrow(RepliFragm) + 1):(i * nrow(RepliFragm))
+  idxFull  <- ((i - 1) * nrow(RepliFull) + 1):(i * nrow(RepliFull))
+  FitDataRepliFragm[idxFragm, 1] <- rep(i, nrow(RepliFragm))
+  FitDataRepliFull[idxFull, 1]  <- rep(i, nrow(RepliFull))
+  FitDataRepliFragm[idxFragm, 2] <- RepliFragm[,i]
+  FitDataRepliFull[idxFull, 2]  <- RepliFull[,i]
+}
+model_Fragm <- lm(FitDataRepliFragm[,2] ~ poly(FitDataRepliFragm[,1], 2))
+model_Full  <- lm(FitDataRepliFull[,2] ~ poly(FitDataRepliFull[,1], 2))
+
 
 # Group chromatin data by overlap
 G2        <- RepliAgg$G2
-WeightedPhase <- as.matrix(RepliAgg[, c("S1", "S2", "S3", "S4", "G2")]) %*% c(1:5)
+S2        <- RepliAgg$S2
+WeightedPhase <- as.matrix(RepliAgg[, OrderedPhases]) %*% 1:6
 RepliByOverlap <- data.frame(
-  Percent_G2 = c(G2[idxOverlap_fragm@queryHits], G2[idxOverlap_Cat@queryHits]),
-  WgtPhase   = c(WeightedPhase[idxOverlap_fragm@queryHits], 
-                 WeightedPhase[idxOverlap_Cat@queryHits]),
-  L1Type     = c(rep("Fragm", length(idxOverlap_fragm@queryHits)),
-                 rep("Full", length(idxOverlap_Cat@queryHits))))
+  Percent_S2 = c(G2[idxOverlap_fragm@from], G2[idxOverlap_Cat@from]),
+  Percent_G2 = c(G2[idxOverlap_fragm@from], G2[idxOverlap_Cat@from]),
+  WgtPhase   = c(WeightedPhase[idxOverlap_fragm@from], 
+                 WeightedPhase[idxOverlap_Cat@from]),
+  L1Type     = c(rep("Fragm", length(idxOverlap_fragm@from)),
+                 rep("Full", length(idxOverlap_Cat@from))))
 boxplot(RepliByOverlap$Percent_G2 ~ RepliByOverlap$L1Type)
 boxplot(RepliByOverlap$WgtPhase ~ RepliByOverlap$L1Type)
+t.test(Percent_S2 ~ L1Type, data = RepliByOverlap)
 t.test(Percent_G2 ~ L1Type, data = RepliByOverlap)
 t.test(WgtPhase ~ L1Type, data = RepliByOverlap)
+hist(RepliByOverlap$WgtPhase[RepliByOverlap$L1Type == "Fragm"])
+hist(RepliByOverlap$WgtPhase[RepliByOverlap$L1Type == "Full"])
 
-HT <- hotelling.test(RepliAgg[idxOverlap_fragm@queryHits, c("G2", "S1", "S2", "S3", "S4")],
-               RepliAgg[idxOverlap_Cat@queryHits, c("G2", "S1", "S2", "S3", "S4")])
-HT
 
 # Distinguish full-length and fragment
 idxOverlap <- findOverlaps(RepliGR, L1RefGR)
-blnFullL1  <- width(L1RefGR[idxOverlap@subjectHits]) > 6000
-boxplot(WeightedPhase[idxOverlap@queryHits] ~ blnFullL1)
-t.test(WeightedPhase[idxOverlap@queryHits] ~ blnFullL1)
+blnFullL1  <- width(L1RefGR[idxOverlap@to]) > 6000
+boxplot(WeightedPhase[idxOverlap@from] ~ blnFullL1)
+t.test(WeightedPhase[idxOverlap@from] ~ blnFullL1)
