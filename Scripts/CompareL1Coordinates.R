8659f6184e0393982ee4345b5a41b9b4825d9c12
hdohna <hb54@aub.edu.lb>
2017-04-16 10:12:31 +0300

    Changes to `CompareL1Coordinates.R`

--- Scripts/CompareL1Coordinates.R
+++ Scripts/CompareL1Coordinates.R
@@ -28,16 +28,16 @@ library(GenomicRanges)
 # Read repeat table and subset to get only L1HS rows with fragment size below 
 # MaxFragLength
 #RepeatTable <- read.delim("D:/L1polymORF/Data/repeatsHg19_L1HS")
-repColNames <- c(paste("X", 1:5, sep = ""), 
-                 c("chromosome", "start", "end", "X9", "strand", "repType", 
-                   "repClass", "repName"), paste("X", 14:17, sep = ""))
-RepeatTable <- read.delim("D:/L1polymORF/Data/rmsk_hg19.txt", header = F,
-                          col.names = repColNames)
-RepeatTable <- RepeatTable[RepeatTable$repName == "L1",]
-nrow(RepeatTable)
-# Create genomic ranges for L1 fragments, match them to distances to get distance
-# to consensus per fragment
-L1RefGR1 <- makeGRangesFromDataFrame(RepeatTable)
+# repColNames <- c(paste("X", 1:5, sep = ""), 
+#                  c("chromosome", "start", "end", "X9", "strand", "repType", 
+#                    "repClass", "repName"), paste("X", 14:17, sep = ""))
+# RepeatTable <- read.delim("D:/L1polymORF/Data/rmsk_hg19.txt", header = F,
+#                           col.names = repColNames)
+# RepeatTable <- RepeatTable[RepeatTable$repName == "L1",]
+# nrow(RepeatTable)
+# # Create genomic ranges for L1 fragments, match them to distances to get distance
+# # to consensus per fragment
+# L1RefGR1 <- makeGRangesFromDataFrame(RepeatTable)
 
 # Import bed file with 
 L1RefGR2 <- import.bed("D:/L1polymORF/Data/hg19.fa_LINE1.bed")
@@ -52,12 +52,19 @@ L1TableGR <- makeGRangesFromDataFrame(L1Table, seqnames.field = "genoName",
 load("D:/L1polymORF/Data/L1RefRanges_hg19.Rdata")
 load("D:/L1polymORF/Data/BZ_L1Ranges.Rdata")
 SuspectL1Ranges
+
+# Create ranges for previously called non-reference insertions
+PreviousNonRefL1GR <- GRanges(seqnames = c("chr12", "chr6", "chr7", "chr7", "chrX"),
+                              IRanges(start = c(28918987, 157968729, 8019012, 156702046, 71811423),
+                                      end = c(28918988, 157968730, 8019013, 156702047, 71811424)))
+
 ###############################################
 #                                             #
 #  Compare different LINE-1 data source       #
 #                                             #
 ###############################################
 
+Dist2Closest(PreviousNonRefL1GR, L1TableGR)
 # Get GRanges that are unique to each dataset
 blnL1RefGR1inGR2 <- overlapsAny(L1RefGR1, L1RefGR2)
 blnL1RefGR2inGR1 <- overlapsAny(L1RefGR2, L1RefGR1)
