67e36de51822fd2df6ac6e29571a50fb07d7d163
hdohna <hb54@aub.edu.lb>
2017-04-19 17:23:58 +0300

    Added status messages to `Create_1000G_L1Table.R`

--- Scripts/Create_1000G_L1Table.R
+++ Scripts/Create_1000G_L1Table.R
@@ -1,4 +1,5 @@
 # The script below subsets creates a table with L1s from the 1000 Genome data
+# Data were downloaded from ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/
 
 # Source start script
 source('/home/hzudohna/L1polymORFgit/Scripts/_Start_L1polymORF_scg4.R')
@@ -7,7 +8,7 @@ source('/home/hzudohna/L1polymORFgit/Scripts/_Start_L1polymORF_scg4.R')
 NrInfoCols <- 9
 DataFolder <- "/srv/gsfs0/projects/levinson/hzudohna/1000Genomes/"
 FilePrefix <- "LINE1"
-OutFile    <- paste(DataFolder, "L1_1000G_withGeno")
+OutFile    <- paste(DataFolder, "L1_1000G_withGenoNum", sep = "")
 
 # function to get the genotype
 GetGenotype <- function(SampleColumn){
@@ -21,13 +22,16 @@ GetGenotype <- function(SampleColumn){
 AllFiles <- list.files(DataFolder, pattern = FilePrefix, full.names = T)
 
 # Loop over file names, read file and append to existing
-L1Table <- read.delim(AllFiles[1], header = F)
+cat("Reading vcf files by chromosome\n")
+L1Table <- read.delim(AllFiles[1], header = F, skip = 1)
 for (L1File in AllFiles[-1]){
-  NewTable <- read.delim(L1File, header = F)
+  cat("Processing", L1File, "\n")
+  NewTable <- read.delim(L1File, header = F, skip = 1)
   L1Table  <- rbind(L1Table, NewTable)
 }
 
 # Get column names from vcf files
+cat("Adding column names to concatenated L1 table\n")
 VcfFiles <- list.files(DataFolder, pattern = "genotypes.vcf", full.names = T)
 VcfFiles <- VcfFiles[-grep("vcf.", VcfFiles)]
 MEI1000GLines <- readLines(VcfFiles[1], n = 300)
@@ -47,12 +51,14 @@ GetGenotype <- function(SampleColumn){
 }
 
 # Turn genotype columns into numeric values
+cat("Converting genotypes into numeric values\n")
 SampleColumns <- colnames(L1Table)[(NrInfoCols + 1):ncol(L1Table)]
 GenoDF <- sapply(SampleColumns, function(x) GetGenotype(L1Table[,x]))
 colnames(GenoDF) <- SampleColumns
 L1Table <- cbind(L1Table[,1:NrInfoCols], GenoDF)
 
 # Extract insertion length
+cat("Extracting insertion length\n")
 L1Table$InsLength <- sapply(as.character(L1Table$INFO), function(x){
   Xsplit <- strsplit(x, ";")[[1]]
   SVLENchar <- grep("SVLEN", Xsplit, value = T)
@@ -64,6 +70,7 @@ L1Table$InsLength <- sapply(as.character(L1Table$INFO), function(x){
 })
 
 # Write out L1Table
+cat("Writing out table\n")
 write.table(L1Table, OutFile)
 
 
