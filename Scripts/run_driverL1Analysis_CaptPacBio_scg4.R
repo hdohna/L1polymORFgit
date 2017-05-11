b5df92fb6de35d9060f66945777a6326ef2599bf
hdohna <hb54@aub.edu.lb>
2017-05-11 17:49:17 +0300

    More options to `run_driverL1Analysis_CaptPacBio_scg4.R`

--- Scripts/run_driverL1Analysis_CaptPacBio_scg4.R
+++ Scripts/run_driverL1Analysis_CaptPacBio_scg4.R
@@ -9,14 +9,14 @@ library(csaw)
 
 # Run function
 driverL1Analysis(
-  PeakBam = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/PacBioL1captPS/BZ_NA12878L1capt5-9kb_subreads_hg19masked.sorted.bam", 
+  PeakBam = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/PacBioL1captPS/PacBioHiFiSubreads_hg19masked_ngmlr.sorted.bam", 
 #  L1HSBamFile = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/BZ_NA12878L1capt5-9kb_sub_reads_aln2L1.sorted.bam", 
   L1HSConsensus = "/srv/gsfs0/projects/levinson/hzudohna/RefSeqData/Homo_sapiens_L1_consensus.fas",
   L1RefRanges    = '/srv/gsfs0/projects/levinson/hzudohna/RefSeqData/L1RefRanges_hg19.Rdata',
   OutputFolder = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/", 
-  ResultFileName = "BZ_NA12878L1capt5-9kb_Results.Rdata",
+  ResultFileName = "PacBioHiFi__Results.Rdata",
   PlotFolder = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/Figures/", 
-  MinMaxCover = 1, 
+  MinMaxCover = 2, 
   MinGap = 100, 
   MinDist2L1 = 500, 
   NrChromPieces = 1,
@@ -29,7 +29,7 @@ driverL1Analysis(
   blnCalcCoverMat          = T,
   IdChar2Remove = 4,
   NrJobsPerBatch = 50, 
-  WaitBetwJobs = 100,
+  WaitBetwJobs = 60,
   NrReadsPerIter = 10^3,
   AlignCommand = c('module load bwa', 'bwa mem -k17 -W40 -r10 -A2 -B5 -O2 -E1 -L0'),
   IndexCommand = c('module load bwa', 'bwa index'),
