3f285e960899b183a4af94cc0710f31167e64b95
hdohna <hb54@aub.edu.lb>
2017-05-24 22:10:28 +0300

    Added script `CalcReadLengths.R`
    Script to calculate read lengths for 2 fastq files.

---
+++ Scripts/CalcReadLengths.R
@@ -0,0 +1,6 @@
+library(ShortRead)
+RL1 <- getReadLengthFromFastQ("/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/PacBioL1captPS/filtered_subreads.fastq")
+RL2 <- getReadLengthFromFastQ("/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/FastqSubreads/BZ_NA12878L1capt5-9kb_sub_reads.fastq")
+cat("******** saving results to /srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/PacBioReadLengths.RData *********\n")
+save.image(file = "/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/PacBioReadLengths.RData")
+
