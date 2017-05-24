e27852e9abf2d62dc587f1970236b10c033148b8
hdohna <hb54@aub.edu.lb>
2017-05-24 22:17:49 +0300

    Bugfix in CalcReadLengths

--- Scripts/CalcReadLengths.R
+++ Scripts/CalcReadLengths.R
@@ -1,5 +1,5 @@
 # Source start script
-source('D:/L1polymORFgit/Scripts/_Start_L1polymORF.R')
+source('/home/hzudohna/L1polymORFgit/Scripts/_Start_L1polymORF_scg4.R')
 
 RL1 <- getReadLengthFromFastQ("/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/PacBioL1captPS/filtered_subreads.fastq")
 RL2 <- getReadLengthFromFastQ("/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/FastqSubreads/BZ_NA12878L1capt5-9kb_sub_reads.fastq")
