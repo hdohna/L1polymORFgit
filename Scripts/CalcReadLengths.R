042d1d895a260c0dfca7adca70908668a6030f98
hdohna <hb54@aub.edu.lb>
2017-05-24 22:14:46 +0300

    changes to `CalcReadLengths.R`

--- Scripts/CalcReadLengths.R
+++ Scripts/CalcReadLengths.R
@@ -1,4 +1,6 @@
-library(ShortRead)
+# Source start script
+source('D:/L1polymORFgit/Scripts/_Start_L1polymORF.R')
+
 RL1 <- getReadLengthFromFastQ("/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/PacBioL1captPS/filtered_subreads.fastq")
 RL2 <- getReadLengthFromFastQ("/srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/FastqSubreads/BZ_NA12878L1capt5-9kb_sub_reads.fastq")
 cat("******** saving results to /srv/gsfs0/projects/levinson/hzudohna/PacBioCapture/PacBioReadLengths.RData *********\n")
