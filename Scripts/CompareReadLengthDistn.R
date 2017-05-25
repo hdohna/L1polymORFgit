13c8579ebe5a2cba35af5c773d38abd459972705
hdohna <hb54@aub.edu.lb>
2017-05-25 21:55:47 +0300

    Changed legend in `CompareReadLengthDistn.R`

--- Scripts/CompareReadLengthDistn.R
+++ Scripts/CompareReadLengthDistn.R
@@ -13,9 +13,9 @@ HistNormal <- hist(RL2, plot = F, breaks = seq(0, 50000, 500))
 plot(HistPS$mids - 50, HistPS$density, type = "s",
      col = "red", ylim = c(0, 0.0006), xlim = c(0, 15000),
      xlab = "Read length", ylab = "Frequency")
-legend("topright", legend = c("Old polymerase", "New polymerase"), lty = c(1, 1),
-       col = c("blue", "red"))
 lines(HistNormal$mids + 50, HistNormal$density, type = "s", col = "blue")
+legend(x = 10000, y = 5.5*10^-4, legend = c("Old polymerase", "New polymerase"), lty = c(1, 1),
+       col = c("blue", "red"), bty = "n")
 CreateDisplayPdf("D:/L1polymORF/Figures/ReadLDistns.pdf",
                  PdfProgramPath = '"C:\\Program Files (x86)\\Adobe\\Reader 11.0\\Reader\\AcroRd32"',
                  height = 7)
