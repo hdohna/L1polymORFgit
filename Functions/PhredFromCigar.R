bf5be7b71d1f08d7febc2bf62b2a98fd461405e9
hdohna <hb54@aub.edu.lb>
2017-05-26 17:31:13 +0300

    Added function `PhredFromCigar.R`
    Calculates phred score aligned to reference.

---
+++ Functions/PhredFromCigar.R
@@ -0,0 +1,34 @@
+# General description:
+#
+#    This function determines the aligned phred score of a read from the cigar 
+#    string and the original phred score sequence
+
+# Arguments:
+#   
+#    CigarString: sam file cigar string
+#    Phred: character string of phred score
+
+# Output:
+#   
+#    PhredVector: aligned sequence as vector of upper case letters
+
+# Comment:
+
+PhredFromCigar <- function(CigarString, Phred){
+  PhredV <- strsplit(as.character(Phred), "")[[1]]
+  Nrs  <- as.numeric(strsplit(CigarString, "[A-Z]")[[1]])
+  Ltrs <- strsplit(CigarString, "[0-9]")[[1]]
+  Ltrs <- Ltrs[Ltrs != ""]
+  StartV <- if(Ltrs[1] == "S") 2 else 1
+  EndV   <- if(Ltrs[length(Ltrs)] == "S") length(Ltrs) - 1 else length(Ltrs)
+  StartS <- if(Ltrs[1] == "S") Nrs[1] + 1 else 1
+  PhredVector <- NULL
+  for (i in StartV:EndV){
+    Phred2Append <- switch(Ltrs[i], M = PhredV[StartS:(StartS + Nrs[i] - 1)],
+                         D = rep("-", Nrs[i]),
+                         I = NULL)
+    PhredVector <- c(PhredVector, Phred2Append)
+    StartS    <- StartS + Nrs[i] * (Ltrs[i] != "D")
+  }
+  PhredVector
+}
