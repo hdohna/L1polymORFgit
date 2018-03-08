651ed13027b611b83c68f91c638b8f5c7f0542f7
hdohna <hb54@aub.edu.lb>
2018-03-08 12:37:12 +0200

    Added function `CreateAndCallqsubScript_scg4.R`
    Function uses scripts headers for scg4.

---
+++ Functions/CreateAndCallqsubScript_scg4.R
@@ -0,0 +1,42 @@
+##############################################
+#
+# General description:
+#
+#   The following function creates and calls a qsub script
+
+# Input:
+#
+#     file: the path to save the script file to
+#     qsubHeaderLines: header lines of pbs script
+#     qsubCommandLines: command lines of pbs script
+#     scriptName: name to be used to identify run
+#     Args: external input arguments
+
+# Output:
+#   
+
+
+# Comments:
+#    
+
+##############################################
+
+
+CreateAndCallqsubScript <- function(file,
+   qsubHeaderLines = c('#! /bin/sh', '#', '#$ -N TEST', '#', '#$ -cwd', '#', 
+                       '#$ -l h_rt=72:00:00', '#', '#$ -j y', '#',
+                       '#$ -P large_mem', '#',
+                       '#$ -S /bin/bash', '#', ''), 
+   qsubCommandLines, scriptName = 'NoName', Args = ""){
+  
+  # Replace name in header lines
+  qsubHeaderLines[grep("-N", qsubHeaderLines)] <- paste('#$ -N', scriptName)
+  
+  # Save script
+  writeLines(c(qsubHeaderLines, qsubCommandLines), file)
+  
+  # Run script
+  RunCmd <- paste("qsub", file, Args)
+  system(RunCmd, wait = F)
+  
+}
