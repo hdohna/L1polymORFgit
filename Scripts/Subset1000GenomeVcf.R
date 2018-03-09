d0db4c4f56202b7435dde83fabeab5ec39f27cc7
hdohna <heinrich.dohna@gmail.com>
2018-03-09 12:44:28 +0200

    Changes to `Subset1000GenomeVcf.R`

--- Scripts/Subset1000GenomeVcf.R
+++ Scripts/Subset1000GenomeVcf.R
@@ -13,11 +13,12 @@ FilePrefix     <- "LINE1"
 # Example file name: ALL.chr2.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf
 AllFiles <- list.files(DataFolder, pattern = ".vcf", full.names = T)
 AllFiles <- AllFiles[-grep("vcf.", AllFiles)]
+InFile <- AllFiles[11]
 for (InFile in AllFiles){
   InFileSplit <- strsplit(InFile, "\\.")[[1]]
   OutFile     <- paste(c(InFileSplit[1:2], ".vcf"), collapse = "")
   OutFile     <- gsub("ALL", FilePrefix, OutFile)
   Cmd <- paste("grep", LineIdentifier,  InFile, ">", OutFile)
   cat("Filtering", InFile, "\n\n")
-  system(Cmd)
+  CreateAndCallqsubScript_scg4(file = "grepScript", qsubCommandLines = Cmd)
 }
