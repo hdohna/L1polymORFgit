572a8253eaf0d50e18f7b759ef9d5ce2733af53b
hdohna <heinrich.dohna@gmail.com>
2018-04-24 11:46:54 +0300

    Change to script `NeighborVar_awk_cmds`

--- Scripts/Download1000GenomeExample.R
+++ Scripts/Download1000GenomeExample.R
@@ -1,18 +1,24 @@
 # Source start script
-source('/home/hzudohna/L1polymORF/Scripts/_Start_L1polymORF_bix2.R')
+source('/home/hzudohna/L1polymORFgit/Scripts/_Start_L1polymORF_scg4.R')
 
 # Read in table with 1000 genome files
+VcfFileTable <- read.table("D:/L1polymORF/Data/vcfFiles1000G", as.is = T)
 VcfFileTable <- read.table("/srv/gsfs0/projects/levinson/hzudohna/1000Genomes/vcfFiles1000G", as.is = T)
 VcfFiles <- VcfFileTable[ ,ncol(VcfFileTable)]
 
 # Create an rsync command
 i <- 2
-SourcePath <- paste("rsync://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/",
+SourcePath <- paste("ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/",
                     VcfFiles[i], sep = "")
 DestPath <- paste("./srv/gsfs0/projects/levinson/hzudohna/1000Genomes/",
                   VcfFiles[i], sep = "")
-RSyncCMD <- paste("rsync -a -P", SourcePath, DestPath)
+#RSyncCMD <- paste("rsync -a -P", SourcePath, DestPath)
+lftpCMD <- c("lftp",
+             paste("open", SourcePath),
+             paste("get", VcfFiles[i], "-o", DestPath),
+             "close",
+             "exit")
 
 # Create and call a script file
-CreateAndCallqsubScript(file = "/home/rsyncExample", 
-                        qsubCommandLines = RSyncCMD)
+CreateAndCallqsubScript(file = "/home/hzudohna/lftpExample", 
+                        qsubCommandLines = lftpCMD, blnWait = T)
--- Scripts/Download1000Genome_rsync.R
+++ Scripts/Download1000Genome_rsync.R
@@ -1,6 +1,10 @@
 rsync -a -P rsync://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ ./
   
-rsync -a -P rsync://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ ./srv/gsfs0/projects/levinson/hzudohna/1000Genomes
+rsync -a -P rsync://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ ./srv/gsfs0/projects/levinson/hzudohna/1000Genomes/
 
 
 ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
+
+
+rsync -P rsync://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr10.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz ./srv/gsfs0/projects/levinson/hzudohna/1000Genomes/
+rsync --copy-links --times --verbose rsync://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr10.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz ./
