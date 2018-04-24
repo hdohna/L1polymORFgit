d04444a50e59469ebcc64c9db016977e60168c38
hdohna <hb54@aub.edu.lb>
2018-04-24 17:52:16 +0300

    Bugfix in script `CalculateSingletonDensityPerL1.R`

--- Scripts/CalculateSingletonDensityPerL1.R
+++ Scripts/CalculateSingletonDensityPerL1.R
@@ -23,12 +23,14 @@ library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 # Specify file paths
 DataPath <- 'D:/L1polymORF/Data/'
 G1000SamplePath <- 'D:/L1polymORF/Data/1000GenomeSampleInfo.txt'
+GapPath         <- 'D:/L1polymORF/Data/Gap_hg19.txt'
 L1GRPath        <- 'D:/L1polymORF/Data/GRanges_L1_1000Genomes.RData'
 L1RefRangePath  <- 'D:/L1polymORF/Data/L1RefRanges_hg19.Rdata'
 ChrLPath        <- 'D:/L1polymORF/Data/ChromLengthsHg19.Rdata'
+HiCFolderPath   <- 'D:/L1polymORF/Data/HiCData/'
 
 # Number of info columns in vcf file
-NrInfoCols <- 9
+NrInfoCols   <- 9
 
 # Minimum number of carriers for a LINE-1 to be analyzed
 MinNrCarrier <- 5
@@ -42,6 +44,11 @@ MinNrCarrier <- 5
 # Load previously generated objects
 load(L1GRPath)
 load(ChrLPath)
+
+# Read information about 1000 genome samples
+SampleInfo  <- read.table(G1000SamplePath, header = T)
+SampleMatch <- match(SampleColumns, SampleInfo$sample)
+Pops        <- SampleInfo$pop[SampleMatch]
 
 ##########
 # Loop over chromosomes and estimate coefficients
@@ -88,28 +95,45 @@ for (Chr in c(1:22, "X")) {
   
   # Loop over L1 with enough carriers and estimate effect of L1 on singleton
   # densities
+  i <- idxEnough[1]
   L1Coeffs <- sapply (idxEnough, function(i) {
     cat("Processing L1", which(idxEnough == i), "out of", length(idxEnough), "\n")
+    
+    # Indicators for singletons that are before and after current L1
     blnBeforeL1 <- Singletons_rev$V2 < Line1Vcf$V2[i]
     blnAfterL1  <- Singletons$V2     > Line1Vcf$V2[i]
+    
+    # Index for L1 carriers on first abd second allele
     idxCarrier1 <- grep("1\\|", Line1Vcf[i,blnSampleCols])
     idxCarrier3 <- grep("\\|1", Line1Vcf[i,blnSampleCols])
-    CarrierMatch1After  <- match(idxSCols, SCols$Col[blnSingl1 & blnAfterL1])
-    CarrierMatch1Before <- match(idxSCols, SCols_rev$Col[blnSingl1 & blnBeforeL1])
-    CarrierMatch3After  <- match(idxSCols, SCols$Col[blnSingl3 & blnAfterL1])
-    CarrierMatch3Before <- match(idxSCols, SCols_rev$Col[blnSingl3 & blnBeforeL1])
-    DistBefore1 <- Line1Vcf$V2[i] - Singletons_rev$V2[CarrierMatch1Before]
-    DistAfter1  <- Singletons_rev$V2[CarrierMatch1Before] - Line1Vcf$V2[i]
-    DistBefore3 <- Line1Vcf$V2[i] - Singletons$V2[CarrierMatch3Before]
-    DistAfter3  <- Singletons$V2[CarrierMatch3Before] - Line1Vcf$V2[i]
     
+    # Match singleton entries for all samples
+    idxA1 <- which(blnSingl1 & blnAfterL1)
+    idxB1 <- which(blnSingl1 & blnBeforeL1)
+    idxA3 <- which(blnSingl3 & blnAfterL1)
+    idxB3 <- which(blnSingl3 & blnBeforeL1)
+    SampleMatch1After  <- match(idxSCols, SCols$Col[idxA1])
+    SampleMatch1Before <- match(idxSCols, SCols_rev$Col[idxB1])
+    SampleMatch3After  <- match(idxSCols, SCols$Col[idxA3])
+    SampleMatch3Before <- match(idxSCols, SCols_rev$Col[idxB3])
+    
+    # Determine distance from L1 to singleton
+    DistBefore1 <- Line1Vcf$V2[i] - Singletons_rev$V2[idxB1[SampleMatch1Before]]
+    DistAfter1  <- Singletons$V2[idxA1[SampleMatch1After]] - Line1Vcf$V2[i]
+    DistBefore3 <- Line1Vcf$V2[i] - Singletons_rev$V2[idxB3[SampleMatch3Before]]
+    DistAfter3  <- Singletons$V2[SampleMatch3After] - Line1Vcf$V2[i]
+    
+    # Create indicator for L1 presence
     L1_1 <- rep(0, length(SampleColumns))
     L1_1[idxCarrier1] <- 1
     L1_3 <- rep(0, length(SampleColumns))
     L1_3[idxCarrier3] <- 1
+    
+    # Create censoring column
     CensorBefore <- rep(1, length(SampleColumns))
     CensorAfter  <- rep(1, length(SampleColumns))
     
+    # Replace NA distances by distance to end
     DistBefore  <- c(DistBefore1, DistBefore3)
     blnNABefore <- is.na(DistBefore)
     CensorBefore[blnNABefore] <- 0
@@ -127,9 +151,9 @@ for (Chr in c(1:22, "X")) {
                     rep("After",  length(DistAfter)))
     SampleIDs  <- rep(idxSCols, 4)
     
-    CPH <- coxph(SurvObj ~ L1 + strata(Direction) + cluster(SampleIDs))
+    CPH <- coxph(SurvObj ~ L1 + strata(Direction) + cluster(SampleIDs) + rep(Pops, 4))
     SU <- summary(CPH)
-    Coeffs <- SU$coefficients
+    Coeffs <- SU$coefficients["L1",]
     names(Coeffs) <- colnames(SU$coefficients)
     Coeffs
   }) 
@@ -145,8 +169,135 @@ for (Chr in c(1:22, "X")) {
   L1Coeffs$Freq      <- L1_1000G_reduced$Frequency[CHrPos_match]
   
   L1SingletonCoeffs <- rbind(L1SingletonCoeffs, L1Coeffs)
-  plot(L1Coeffs$InsLength, L1Coeffs$coef, main = paste("chr", Chr))
+#  plot(L1Coeffs$InsLength, L1Coeffs$coef, main = paste("chr", Chr))
   
 }
-#plot(L1Coeffs$Freq, L1Coeffs$coef)
+
+##########################################
+#                                        #
+#        Plot coefficients               #
+#                                        #
+##########################################
+
+# Plot coefficients vs insertion length
+plot(L1SingletonCoeffs$InsLength, L1SingletonCoeffs$coef, 
+     xlab = "Insertion length", ylab = "Singleton coefficient")
+blnSig <- L1SingletonCoeffs$Pr...z.. < 0.05
+points(L1SingletonCoeffs$InsLength[blnSig], L1SingletonCoeffs$coef[blnSig], 
+       col = "red")
+L1SingletonCoeffs$InsLBins <- cut(L1SingletonCoeffs$InsLength, breaks = seq(0, 6500, 1000))
+MeanCoeffPerL <- aggregate(L1SingletonCoeffs[,c("coef", "InsLength")],
+                           by = list(L1SingletonCoeffs$InsLBins), FUN = mean)
+plot(MeanCoeffPerL$InsLength, MeanCoeffPerL$coef)
+
+##########################################
+#                                        #
+#        Regress coefficients            #
+#                                        #
+##########################################
+
+# Make genomic ranges for L1SingletonCoeffs
+L1SingletonCoeffs$chromosome <- paste("chr", L1SingletonCoeffs$Chrom, sep = "")
+L1SingletonCoeffs_GR <- makeGRangesFromDataFrame(L1SingletonCoeffs, 
+                                                 seqnames.field = "chromosome",
+                                                 start.field = "Pos",
+                                                 end.field = "Pos")
+
+# Caclulate distance to genes
+L1SingletonCoeffs$Dist2Gene <- Dist2Closest(L1SingletonCoeffs_GR, 
+                                            genes(TxDb.Hsapiens.UCSC.hg19.knownGene))
+
+#######
+# Regress against distance to genes
+#######
+
+# Regress coefficient against distance to genes
+LM_Full <- lm(coef ~ Dist2Gene, data = L1SingletonCoeffs, 
+         subset = L1SingletonCoeffs$InsLength >= 6000 & L1SingletonCoeffs$robust.se > 0,
+         weights = 1/L1SingletonCoeffs$robust.se)
+summary(LM_Full)
+
+LM_Fragm <- lm(coef ~ Dist2Gene, data = L1SingletonCoeffs, 
+              subset = L1SingletonCoeffs$InsLength < 5900 & L1SingletonCoeffs$robust.se > 0,
+              weights = 1/L1SingletonCoeffs$robust.se)
+summary(LM_Fragm)
+
+##########
+# Process loops
+##########
+
+# Auxiliary unction to create genomic ranges for both sides of a loop
+getLoopGRs <- function(FileName){
+  Loops <- read.delim(FileName)
+  Loops$chr1 <- paste("chr", Loops$chr1, sep = "")
+  Loops$chr2 <- paste("chr", Loops$chr2, sep = "")
+  LoopsGR1 <- makeGRangesFromDataFrame(Loops, seqnames.field = "chr1", 
+                                       start.field="x1", end.field = "x2")
+  LoopsGR2 <- makeGRangesFromDataFrame(Loops, seqnames.field = "chr2", 
+                                       start.field="y1", end.field = "y2")
+  blnOverlapLoop <- overlapsAny(LoopsGR1, LoopsGR2)
+  AllLoops <- c(LoopsGR1, LoopsGR2[!blnOverlapLoop])
+  GRangesList(LoopsGR1 = LoopsGR1, LoopsGR2 = LoopsGR2, AllLoops = AllLoops)
+}
+
+# List of files with domains and loops
+DomainFiles <- list.files(HiCFolderPath, pattern = "domainlist.txt",
+                          full.names = T)
+DomainFiles <- DomainFiles[! DomainFiles %in% grep(".gz", DomainFiles, value = T)]
+LoopFiles <- list.files(HiCFolderPath, pattern = "looplist.txt",
+                        full.names = T)
+LoopFiles <- LoopFiles[! LoopFiles %in% grep(".gz", LoopFiles, value = T)]
+
+# Get list of domain and loop ranges
+DomainGRList   <- lapply(DomainFiles, getLoopGRs)
+LoopGRList     <- lapply(LoopFiles, getLoopGRs)
+names(DomainGRList) <- sapply(DomainFiles, 
+                              function(x) strsplit(x, "_")[[1]][2])
+names(LoopGRList) <- sapply(LoopFiles, 
+                            function(x) strsplit(x, "_")[[1]][2])
+
+# Summarize domain ranges (union or intersect)
+DomainGR_Intersect <- DomainGRList[[1]]$AllLoops
+LoopGR_Intersect   <- LoopGRList[[1]]$AllLoops
+for (i in 2:length(LoopGRList)){
+  DomainGR_Intersect <- intersect(DomainGR_Intersect, DomainGRList[[i]]$AllLoops)
+  LoopGR_Intersect   <- intersect(LoopGR_Intersect, LoopGRList[[i]]$AllLoops)
+}
+
+# Determine distance to loops and domains
+L1SingletonCoeffs$Dist2Loop <- Dist2Closest(L1SingletonCoeffs_GR, 
+                                            LoopGR_Intersect)
+L1SingletonCoeffs$Dist2Domain <- Dist2Closest(L1SingletonCoeffs_GR, 
+                                              DomainGR_Intersect)
+
+#######
+# Regress against distance to loops
+#######
+
+# Regress coefficient against distance to loops 
+LM_Full <- lm(coef ~ Dist2Loop, data = L1SingletonCoeffs, 
+              subset = L1SingletonCoeffs$InsLength >= 6000 & L1SingletonCoeffs$robust.se > 0,
+              weights = 1/L1SingletonCoeffs$robust.se)
+summary(LM_Full)
+
+LM_Fragm <- lm(coef ~ Dist2Loop, data = L1SingletonCoeffs, 
+               subset = L1SingletonCoeffs$InsLength < 5900 & L1SingletonCoeffs$robust.se > 0,
+               weights = 1/L1SingletonCoeffs$robust.se)
+summary(LM_Fragm)
+
+#######
+# Regress against distance to loops and genes
+#######
+
+# Regress coefficient against distance to loops 
+LM_Full <- lm(coef ~ Dist2Loop + Dist2Gene, data = L1SingletonCoeffs, 
+              subset = L1SingletonCoeffs$InsLength >= 6000 & L1SingletonCoeffs$robust.se > 0,
+              weights = 1/L1SingletonCoeffs$robust.se)
+summary(LM_Full)
+
+LM_Fragm <- lm(coef ~ Dist2Loop + Dist2Gene, data = L1SingletonCoeffs, 
+               subset = L1SingletonCoeffs$InsLength < 5900 & L1SingletonCoeffs$robust.se > 0,
+               weights = 1/L1SingletonCoeffs$robust.se)
+summary(LM_Fragm)
+
 
