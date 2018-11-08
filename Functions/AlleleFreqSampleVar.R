cb064023e315a76ecd6131710725d74b362df106
hdohna <heinrich.dohna@gmail.com>
2018-11-08 22:53:08 +0200

    Change in function `AlleleFreqSampleVar.R`
    Function to estimate parameters of a selection coefficient distribution.

--- Functions/AlleleFreqSampleVar.R
+++ Functions/AlleleFreqSampleVar.R
@@ -17,45 +17,31 @@
 
 ##############################################
 
-AlleleFreqSampleVar <- function(k, m, SD, N, SampleSize = 2504, blnUseFPrime = T){
-    
-  # The lines below use F' in Boissinot's paper
-  if (blnUseFPrime){
+AlleleFreqSampleVar <- function(k, m, SD, N, SampleSize = 2504){
     
     # Calculate integration constant
     IntConst <- integrate(function(s){
-      dnorm(s, m, SD) * integrate(function(x) AlleleFreqTime(x, s, N), 
+      dnorm(s, m, SD) * (
+        integrate(function(x) AlleleFreqTime(x, s, N), 
                 0, 1)$value - 
         integrate(function(x) (1 - x)^SampleSize * AlleleFreqTime(x, s, N), 
                   0, 1)$value
-      
+      )
     }, -1, 1)$value
     
     
     # Calculate probability of obtaining k alleles in a sample of size 
     # SampleSize
-    # lchoose(SampleSize, k) +
-    #   log(integrate(function(x) AlleleFreqTime(x, s, N) * x^k * 
-    #                   (1 - x)^(SampleSize - k), 0, 1)$value -
-    #   integrate(function(x) (1 - x)^SampleSize * AlleleFreqTime(x, s, N) * x^k * 
-    #   (1 - x)^(SampleSize - k), 0, 1)$value) - log(IntConst)
-
-      log(integrate(function(x) AlleleFreqTime(x, s, N)*dbinom(k, SampleSize, x), 
+    log(integrate(function(s){
+      dnorm(s, m, SD) * (
+        integrate(function(x) AlleleFreqTime(x, s, N)*dbinom(k, SampleSize, x), 
                     0, 1)$value -
-            integrate(function(x) (1 - x)^SampleSize * AlleleFreqTime(x, s, N)*
-                        dbinom(k, SampleSize, x), 0, 1)$value) - log(IntConst)
-  } else {
-    
-    # Calculate integration constant
-    IntConst <- integrate(function(x) AlleleFreqTime(x, s, N), 0, 1)$value
-    
-    # Calculate probability of obtaining k alleles in a sample of size 
-    # SampleSize
-    log(integrate(function(x) AlleleFreqTime(x, s, N) *
-                      dbinom(k, SampleSize, x), 0, 1)$value) - log(IntConst)
-  }
-  
-  
+        integrate(function(x) (1 - x)^SampleSize * AlleleFreqTime(x, s, N)*
+                        dbinom(k, SampleSize, x), 0, 1)$value
+      )
+    }, -1, 1)$value)
+
+    - log(IntConst)
 }
 
 
