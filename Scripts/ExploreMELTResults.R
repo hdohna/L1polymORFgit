f5e42b7dd014ddcffde36aaf3cf49452d3df2736
hdohna <heinrich.dohna@gmail.com>
2019-02-07 10:07:47 +0200

    Change in L1 selection estimation

--- Scripts/ExploreMELTResults.R
+++ Scripts/ExploreMELTResults.R
@@ -8,6 +8,7 @@ MEVarCall <- read.table("D:/L1polymORF/Data/nstd144.GRCh37.variant_call.vcf",
                         as.is = T,
                         col.names = c("Chrom", "Pos", "ID", "Alt", "Type", "V6", 
                                       "V7", "Info"))
+MEVarCall$Info[1:10]
 MEVarRegion <- read.table("D:/L1polymORF/Data/nstd144.GRCh37.variant_region.vcf", as.is = T)
 table(MEVarRegion$V5)
 MEVarRegion$V8[1]
@@ -28,12 +29,24 @@ idxIns[1]
 MEVarCall$Info[idxIns[1]]
 
 # Extract allele frequency from info column
+x <- MEVarCall$Info[idxIns[1]]
 GetAF <- function(x){
   xSplit <- strsplit(x, ";")[[1]]
   AFch   <- strsplit(xSplit[length(xSplit)], "=")[[1]][2]
   as.numeric(AFch)
 }
 MEVarCall$Freq <- sapply(MEVarCall$Info, GetAF)
+
+GetInsLength <- function(x){
+  xSplit <- strsplit(x, ";")[[1]]
+  SVLENPart <- grep("SVLEN=", xSplit, value = T)
+  SVLEN   <- strsplit(SVLENPart, "=")[[1]][2]
+  as.numeric(SVLEN)
+}
+MEVarCall$InsLength <- sapply(MEVarCall$Info, GetInsLength)
+hist(MEVarCall$InsLength[idxIns])
+
+MEVarCall$Info[which(MEVarCall$InsLength < 0)[1]]
 
 # Create GRanges object for MEVarCall
 MEVarCall$ChromName <- paste("chr", MEVarCall$Chrom, sep = "")
@@ -49,8 +62,106 @@ plot(L1_1000G_reduced$Frequency[OL_1000G_MEVar@from],
      MEVarCall$Freq[OL_1000G_MEVar@to])
 lines(c(0, 1), c(0, 1))
 
+plot(log(L1_1000G_reduced$Frequency[OL_1000G_MEVar@from]), 
+     log(MEVarCall$Freq[OL_1000G_MEVar@to]), col = rgb(0, 0, 0, alpha = 0.2),
+     pch = 16)
+lines(c(-10, 1), c(-10, 1), col = "red")
+FreqSmu <- supsmu(log(L1_1000G_reduced$Frequency[OL_1000G_MEVar@from]), 
+     log(MEVarCall$Freq[OL_1000G_MEVar@to]))
+lines(FreqSmu$x, FreqSmu$y, col = "blue")
+
 NewGR <- resize(MEVar_GR, 100, fix = "center")
 sum(overlapsAny(L1GRanges,  NewGR))
 OL_Ref_MEVar <- findOverlaps(L1GRanges,  MEVar_GR)
 L1GRanges[OL_Ref_MEVar@from[1:10]]
 MEVar_GR[OL_Ref_MEVar@to[1:10]]
+
+#############################################################
+#                                                           #
+#   Plot frequency quantiles vs. insertion length           #
+#         1000 genome data                                  #
+#                                                           #
+#############################################################
+
+# Create a vector of L1 start classes
+L1_1000G$InsLengthClass <- cut(L1_1000G$InsLength, breaks = 
+                                 seq(0, 6500, 500))
+
+# Get mean L1 frequency per start
+L1WidthAggregated <- aggregate(L1_1000G[,c("InsLength", "Frequency")], 
+                               by = list(L1_1000G$InsLengthClass), FUN = mean)
+L1WidthAggregated_var <- aggregate(L1_1000G[,c("InsLength", "Frequency")], 
+                                   by = list(L1_1000G$InsLengthClass), FUN = var)
+L1WidthAggregated_n <- aggregate(L1_1000G[,c("InsLength", "Frequency")], 
+                                 by = list(L1_1000G$InsLengthClass), FUN = length)
+
+# Create matrix of quantiles per length mat
+LengthClasses <- unique(L1_1000G$InsLengthClass)
+
+ProbV <- seq(0, 1, 0.1)
+QMat <- sapply(LengthClasses, function(x){
+  idxL1 <- which(L1_1000G$InsLengthClass == x)
+  quantile(L1_1000G$Frequency[idxL1], ProbV)
+})
+QMat <- QMat[,-ncol(QMat)]
+QMatLog <- log(QMat)
+QMatLog[is.infinite(QMatLog)] <- -10
+dim(L1WidthAggregated)
+Cols <- rainbow(nrow(QMatLog))
+par( mfrow = c(1, 1), oma = c( 0.2,  0.2,  0.2,  0.2), 
+     mai = c(1, 1, 0.2, 1),
+     cex.lab = 1)
+i <- 1
+plot(L1WidthAggregated$InsLength, QMatLog[i,], ylim = c(-10, 0),
+     type = "l", col = Cols[i])
+for (i in 2:nrow(QMatLog)){
+  lines(L1WidthAggregated$InsLength, QMatLog[i,], col = Cols[i])
+}
+
+CreateDisplayPdf('D:/L1polymORF/Figures/FreqVsL1Width_quantiles.pdf',
+                 PdfProgramPath = '"C:\\Program Files (x86)\\Adobe\\Reader 11.0\\Reader\\AcroRd32"',
+                 height = 5, width = 5)
+
+
+#############################################################
+#                                                           #
+#   Plot frequency quantiles vs. insertion length           #
+#         1000 genome data                                  #
+#                                                           #
+#############################################################
+
+# Create a vector of L1 start classes
+MEVarCall$InsLengthClass[idxIns] <- cut(MEVarCall$InsLength[idxIns], breaks = 
+                                 seq(0, 6500, 500))
+
+# Get mean L1 frequency per start
+L1WidthAggregated <- aggregate(MEVarCall[idxIns,c("InsLength", "Freq")], 
+                               by = list(MEVarCall$InsLengthClass[idxIns]), FUN = mean)
+L1WidthAggregated_var <- aggregate(MEVarCall[idxIns,c("InsLength", "Freq")], 
+                                   by = list(MEVarCall$InsLengthClass[idxIns]), FUN = var)
+L1WidthAggregated_n <- aggregate(MEVarCall[idxIns,c("InsLength", "Freq")], 
+                                 by = list(MEVarCall$InsLengthClass[idxIns]), FUN = length)
+
+# Create matrix of quantiles per length mat
+LengthClasses <- unique(MEVarCall$InsLengthClass[idxIns])
+
+ProbV <- seq(0, 1, 0.25)
+QMat <- sapply(LengthClasses, function(x){
+  idxL1 <- which(MEVarCall$InsLengthClass == x)
+  quantile(MEVarCall$Freq[idxL1], ProbV, na.rm = T)
+})
+as.numeric(MEVarCall$Freq)
+QMat <- QMat[,-ncol(QMat)]
+QMatLog <- log(QMat)
+QMatLog[is.infinite(QMatLog)] <- -10
+dim(L1WidthAggregated)
+Cols <- rainbow(nrow(QMatLog))
+par( mfrow = c(1, 1), oma = c( 0.2,  0.2,  0.2,  0.2), 
+     mai = c(1, 1, 0.2, 1),
+     cex.lab = 1)
+i <- 1
+plot(L1WidthAggregated$InsLength, QMatLog[i,], ylim = c(-10, 0),
+     type = "l", col = Cols[i])
+for (i in 2:nrow(QMatLog)){
+  lines(L1WidthAggregated$InsLength, QMatLog[i,], col = Cols[i])
+}
--- Scripts/run_MELT_Del_1000G.R
+++ Scripts/run_MELT_Del_1000G.R
@@ -17,7 +17,7 @@ Path1000G <- "/labs/dflev/hzudohna/1000Genomes/"
 load(paste(Path1000G, 'GRanges_L1_1000Genomes.RData', sep = ""))
 load(paste(RefPath, 'L1RefRanges_hg19.Rdata', sep = ""))
 
-# Create bed file of ranges around each L1
+# Create bed file of ranges around each reference L1
 ChrNames <- as.vector(seqnames(L1GRanges))
 ChrNrs   <- substr(ChrNames, 4, nchar(ChrNames))
 L1NeighborRanges <- GRanges(seqnames = ChrNrs, 
@@ -32,6 +32,19 @@ L1Ranges_shortName <- GRanges(seqnames = ChrNrs,
                                       end   = end(L1GRanges)))
 export.bed(L1Ranges_shortName, L1bedPath) 
 
+
+# Create bed file of ranges around each 1000 genome L1
+ChrNames <- as.vector(seqnames(L1_1000G_GR_hg19))
+ChrNrs   <- substr(ChrNames, 4, nchar(ChrNames))
+L1NeighborRanges_1000G <- GRanges(seqnames = ChrNrs, 
+                            IRanges(start = start(L1_1000G_GR_hg19) - 500,
+                                    end   = end(L1_1000G_GR_hg19) + 500))
+L1NeighborbedPath_1000G <- paste(RefPath, "L1NeighborRanges_1000G.bed", sep = "")
+L1bedPath_1000G         <- paste(RefPath, "L1Ranges_1000G.bed", sep = "")
+#export.bed(L1NeighborRanges_1000G, L1NeighborbedPath_1000G) 
+
+export.bed(c(L1Ranges_shortName, L1NeighborRanges_1000G), L1NeighborbedPath_1000G) 
+
 # Specify the general path to 1000 genome bam file
 BamPath1000G_General <- "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/IndividualID/alignment/IndividualID.mapped.ILLUMINA.bwa.GBR.low_coverage.20101123.bam"
 DirPath1000G_General <- "ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/IndividualID/alignment/"
@@ -44,6 +57,7 @@ SampleColumns[blnNotAnalyzed]
 
 ########################################################################################
 # Run MELT for low coverage bam files
+IndividualID = SampleColumns[1]
 for (IndividualID in SampleColumns[blnNotAnalyzed]){
   
   # Define paths
@@ -63,21 +77,26 @@ for (IndividualID in SampleColumns[blnNotAnalyzed]){
     SplitLines[length(SplitLines)]
   })
   FileNames   <- FileNames[grep("\\.mapped", FileNames)]
-  BamFileName <- FileNames[-grep("bam.b", FileNames)]
+  BamFileName <- FileNames[-grep("bam.", FileNames)]
   BamPath1000G <- paste("ftp://", DirPath1000G, BamFileName, sep = "")
 
   # Construct samtools command to get filtered bam file
   SamToolsCmds <- c("module load samtools",
                     paste("samtools view -b -h", BamPath1000G, "-L", 
-                          L1NeighborbedPath, "| samtools sort -o",
+                          L1NeighborbedPath_1000G, "| samtools sort -o",
                           BamOutPath),
                     paste("samtools index", BamOutPath))
   
   # Construct MELT command
-  MELTCmds <- paste("java -Xmx2g -jar /labs/dflev/hzudohna/MELTv2.1.5/MELT.jar Deletion-Genotype -bamfile",
+  MELTCmds <- paste("java -Xmx2g -jar /labs/dflev/hzudohna/MELTv2.1.5/MELT.jar Single -bamfile",
 #                    BamOutPath,"-h /labs/dflev/hzudohna/RefSeqData/hg19masked.fa",
-                    BamOutPath, "-h /labs/dflev/hzudohna/RefSeqData/human_g1k_v37.fasta",
-                    "-bed", L1bedPath,
+                    BamOutPath, 
+                    "-c 7",
+                    "-h /labs/dflev/hzudohna/RefSeqData/hs37d5.fa.gz",
+                    "-t /labs/dflev/hzudohna/MELTv2.1.5/me_refs/1KGP_Hg19/LINE1_MELT.zip",
+                    "-n /labs/dflev/hzudohna/MELTv2.1.5/add_bed_files/1KGP_Hg19/hg19.genes.bed ",
+                    "-b MT/GL000207.1/GL000226.1/GL000229.1/GL000231.1/GL000210.1/GL000239.1/GL000235.1/GL000201.1/GL000247.1/GL000245.1/GL000197.1/GL000203.1/GL000246.1/GL000249.1/GL000196.1/GL000248.1/GL000244.1/GL000238.1/GL000202.1/GL000234.1/GL000232.1/GL000206.1/GL000240.1/GL000236.1/GL000241.1/GL000243.1/GL000242.1/GL000230.1/GL000237.1/GL000233.1/GL000204.1/GL000198.1/GL000208.1/GL000191.1/GL000227.1/GL000228.1/GL000214.1/GL000221.1/GL000209.1/GL000218.1/GL000220.1/GL000213.1/GL000211.1/GL000199.1/GL000217.1/GL000216.1/GL000215.1/GL000205.1/GL000219.1/GL000224.1/GL000223.1/GL000195.1/GL000212.1/GL000222.1/GL000200.1/GL000193.1/GL000194.1/GL000225.1/GL000192.1/NC_007605",
+#                    "-bed", L1bedPath,
                     "-w /labs/dflev/hzudohna/1000Genomes/")
   
   # Command to remove bam files
@@ -87,6 +106,7 @@ for (IndividualID in SampleColumns[blnNotAnalyzed]){
   CreateAndCallSlurmScript(file = ScriptName, 
                            SlurmCommandLines = c(SamToolsCmds, 
                                                  "echo 'bam file retrieved'", 
+                                                 "module load bowtie",
                                                  MELTCmds, 
                                                  "echo 'MELT completed'",
                                                  RemCmds,
