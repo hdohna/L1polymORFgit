738ba58690be83b1b1ae12dd9a1c10ba74561f9b
hdohna <heinrich.dohna@gmail.com>
2019-02-12 12:24:12 +0200

    Change in script for MELT simulation workflow

--- Scripts/run_MELT_Simulations.R
+++ Scripts/run_MELT_Simulations.R
@@ -8,10 +8,17 @@ source('/home/hzudohna/L1polymORFgit/Scripts/_Start_L1polymORF_scg4.R')
 library(Rsamtools)
 library(rtracklayer)
 
+# Boolean variables for different parts of the workflow
+blnRunSim  <- F
+blnRunBWA  <- F
+blnIdxBam  <- T
+blnRunMELT <- T
+
 # Path to reference data and for 1000 genomes
-#RefPath   <- "D:/L1polymORF/Data/"
-RefPath   <- "/labs/dflev/hzudohna/RefSeqData/"
-Path1000G <- "/labs/dflev/hzudohna/1000Genomes/L1_simulation_MELT/"
+#RefPath    <- "D:/L1polymORF/Data/"
+RefPath     <- "/labs/dflev/hzudohna/RefSeqData/"
+RefFilePath <- "/labs/dflev/hzudohna/RefSeqData/hg19.fa"
+Path1000G   <- "/labs/dflev/hzudohna/1000Genomes/L1_simulation_MELT/"
 
 # Load necessary objects
 load(paste(Path1000G, 'GRanges_L1_1000Genomes.RData', sep = ""))
@@ -24,7 +31,7 @@ SimGenomes <- list.files(Path1000G, pattern = ".fa", full.names = T)
 # Simulate and align reads for enome
 SimGenome <- "/labs/dflev/hzudohna/1000Genomes/L1_simulation_MELT//hg19_HG00096.fa"
 
-for (SimGenome in SimGenomes[-1]){
+for (SimGenome in SimGenomes){
   
   # Get ID
   Split1 <- strsplit(SimGenome, "_")[[1]]
@@ -37,6 +44,8 @@ for (SimGenome in SimGenomes[-1]){
   Sai2   <- gsub(".fa", "_2.sai", SimGenome)
   OutSim <- gsub(".fa", "_wgsim_out", SimGenome)
   OutSam <- gsub(".fa", ".sam", SimGenome)
+  OutBam <- gsub(".fa", ".bam", SimGenome)
+  OutBamSorted <- gsub(".fa", "_sorted.bam", SimGenome)
   
   # Construct MELT command
   wgsimCmds <- paste("/home/hzudohna/wgsim/wgsim -e 0.004 -d 379.5 -s 21.6 -1 100 -2 100 -N 145000000",
@@ -44,20 +53,45 @@ for (SimGenome in SimGenomes[-1]){
   
   # Bwa commands
   BWACmds <- c("module load bwa",
-               paste("bwa aln /labs/dflev/hzudohna/RefSeqData/hg19.fa", Fq1, 
+               paste("bwa aln", RefFilePath, Fq1, 
                      paste(">", Sai1)),
-               paste("bwa aln /labs/dflev/hzudohna/RefSeqData/hg19.fa", Fq2, 
+               paste("bwa aln", RefFilePath, Fq2, 
                      paste(">", Sai2)),
-               paste("bwa sampe -a 618 /reference/RefGenomes/H_sapiens/hg19/hg19.fa",
+               paste("bwa sampe -a 618", RefFilePath,
                      Sai1, Sai2, Fq1, Fq2, ">", OutSam))
+  
+  # Sam commands to turn sam file into bam file
+  SamBamCmds <- c("module load samtools",
+                paste("samtools view", OutSam, "-b -h -o", OutBam),
+                paste("rm", OutSam))
 
+  # Sam commands to turn sam file into bam file
+  SamIdxCmds <- c("module load samtools",
+                  paste("samtools sort", OutBam, "-o", OutBamSorted),
+                  paste("samtools index", OutBamSorted))
+
+    # Construct MELT command
+  MELTCmds <- c("module load bowtie",
+                paste("java -Xmx2g -jar /labs/dflev/hzudohna/MELTv2.1.5/MELT.jar Single -bamfile",
+                      OutBamSorted, 
+                      "-c 7",
+                      "-h", RefFilePath,
+                      "-t /labs/dflev/hzudohna/MELTv2.1.5/me_refs/1KGP_Hg19/LINE1_MELT.zip",
+                      "-n /labs/dflev/hzudohna/MELTv2.1.5/add_bed_files/1KGP_Hg19/hg19.genes.bed",
+                      "-b MT/GL000207.1/GL000226.1/GL000229.1/GL000231.1/GL000210.1/GL000239.1/GL000235.1/GL000201.1/GL000247.1/GL000245.1/GL000197.1/GL000203.1/GL000246.1/GL000249.1/GL000196.1/GL000248.1/GL000244.1/GL000238.1/GL000202.1/GL000234.1/GL000232.1/GL000206.1/GL000240.1/GL000236.1/GL000241.1/GL000243.1/GL000242.1/GL000230.1/GL000237.1/GL000233.1/GL000204.1/GL000198.1/GL000208.1/GL000191.1/GL000227.1/GL000228.1/GL000214.1/GL000221.1/GL000209.1/GL000218.1/GL000220.1/GL000213.1/GL000211.1/GL000199.1/GL000217.1/GL000216.1/GL000215.1/GL000205.1/GL000219.1/GL000224.1/GL000223.1/GL000195.1/GL000212.1/GL000222.1/GL000200.1/GL000193.1/GL000194.1/GL000225.1/GL000192.1/NC_007605",
+                      "-w", Path1000G))
+                
+  
+  # Create a list of all commands
+  CmdList <- list(wgsimCmds, c(BWACmds, SamBamCmds), SamIdxCmds, MELTCmds)
+  AllCmds <- unlist(CmdList[c(blnRunSim, blnRunBWA, blnIdxBam, blnRunMELT)])
+  
   # Create script name and run script
-  ScriptName <- paste("Sim_Bwa_Script", ID, sep = "_")
+  ScriptName <- paste("Sim_Bwa_MELTScript", ID, sep = "_")
   CreateAndCallSlurmScript(file = ScriptName, 
-                           SlurmCommandLines = c(wgsimCmds, 
-                                                 "echo 'simulation completed'", 
-                                                 BWACmds,
-                                                 "echo 'alignment completed'"))
+                           RunTime = '12:00:00',
+                           Mem = '100G',
+                           SlurmCommandLines = AllCmds)
   
 }
 
