46036e5ea05d9f99d47cdb05f02a872784fb3dd7
hdohna <heinrich.dohna@gmail.com>
2019-03-18 16:09:37 +0200

    Added function to summarize queue
    Returns number of running and pending jobs

---
+++ Functions/SummarizeQueue.R
@@ -0,0 +1,36 @@
+##############################################
+#
+# General description:
+#
+#   The following checks a slurm queue repeatedly and
+#   returns a boolean whether the queue is finished
+
+# Input:
+#
+#     MaxNrTrials: integer for maximum number of trials
+#     SleepTime: how long to wait until next check
+
+# Output:
+#    QueueFinished: logical indicating whether queue ha finished
+
+
+# Comments:
+#    
+
+##############################################
+
+
+SummarizeQueue <- function(){
+  
+    QueueStatus <- system('squeue -u hzudohna',  intern = T)
+    idxBatch    <- grep("batch", QueueStatus)
+    idxInteract <- grep("interact", QueueStatus)
+    idxRunning  <- grep(" R ", QueueStatus)
+    idxPending  <- grep(" PD ", QueueStatus)
+    cat("******   Batch jobs:   **********\n")
+    cat(length(intersect(idxBatch, idxRunning)), "jobs running and ")
+    cat(length(intersect(idxBatch, idxPending)), "jobs pending\n\n")
+    cat("******   Interactive jobs:   **********\n")
+    cat(length(intersect(idxInteract, idxRunning)), "jobs running and ")
+    cat(length(intersect(idxInteract, idxPending)), "jobs pending\n")
+}
