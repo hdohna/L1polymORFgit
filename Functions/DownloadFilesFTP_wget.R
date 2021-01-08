ad8f03d89214b5a0af4109b949f6dc910aa63872
hdohna <hb54@aub.edu.lb>
2021-01-08 17:32:54 +0200

    Update DownloadFilesFTP_wget.R

--- Functions/DownloadFilesFTP_wget.R
+++ Functions/DownloadFilesFTP_wget.R
@@ -13,20 +13,32 @@ DownloadFilesFTP_wget <- function(FileURL, OutputDir = NULL, FileListPath = "Fil
                               FileExt = ".vcf", ScriptName = "DownloadScript",
                               RunTime = '12:00:00',
                               Mem = '50G') {
+  ScriptName = "DownloadScript"
+  RunTime = '12:00:00'
+  Mem = '50G'
   
+  source('/home/hb54/L1polymORFgit/Scripts/_Start_L1polymORF_AUB.R')
   OutputDir <- "/home/hb54/1000GenomeData"
-  FileURL <- "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/"
+  OutputDir <- "/scratch/hb54/dbSNP"
+    
+  FileURL <- "https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh37p13/VCF/"
   FileListPath <- "1000GenomeFiles"
+  FileListPath <- "dbSNPFiles"
   
   CurlCmd <- paste("curl", FileURL, ">", FileListPath)
   system(CurlCmd)
 
   # Read in table with file names and write out a file with urls 
-  FileTable <- read.table(FileListPath)
-  writeLines(paste0(FileURL, FileTable[,ncol(FileTable)]), FileListPath)
-
+  # FileTable <- read.table(FileListPath)
+  # writeLines(paste0(FileURL, FileTable[,ncol(FileTable)]), FileListPath)
+  FileLines <- readLines(FileListPath)
+  FileNames <- sapply(FileLines, function(x) strsplit(x, "\"")[[1]][2])
+  FileNames <- FileNames[!is.na(FileNames)]
+  FileNames
+  writeLines(paste0(FileURL, FileNames), FileListPath)
+  
   # Create wget command and run a slurm script with get command
-  WgetCmd <- c(paste("cd", OutputDir), paste("wget -i", FileListPath))
+  WgetCmd <- c(paste("cd", OutputDir), paste("wget -i", paste0("/home/hb54/", FileListPath)))
   CreateAndCallSlurmScript(file = ScriptName, 
                            scriptName = ScriptName,
                            SlurmCommandLines = WgetCmd,
