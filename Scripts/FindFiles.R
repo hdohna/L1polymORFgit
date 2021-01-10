18d16d13f237efc99fe16a1df4876e6eaf0ca762
hdohna <heinrich.dohna@gmail.com>
2021-01-10 15:43:47 +0200

    Updated FindFiles

--- Scripts/FindFiles.R
+++ Scripts/FindFiles.R
@@ -3,7 +3,7 @@
 # Source start script
 source('D:/L1polymORFgit/Scripts/_Start_L1polymORF.R')
 
-SearchText("L1RefRanges", FolderPath = "D:/L1polymORF/Scripts")
+SearchText("L1RefRanges", FolderPath = "D:/L1polymORFgit/Scripts")
 SearchText("filtered_subreads", FolderPath = "D:/L1polymORFgit/Scripts")
 SearchText("Turning", FolderPath = "D:/L1polymORF/Functions")
 SearchText("L1CatalogGRanges.RData", FolderPath = "D:/L1polymORFgit/Scripts")
